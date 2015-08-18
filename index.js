0b82ea632de832e24d522e1277072eeead7f9784
Allain Lalonde <allain.lalonde@gmail.com>
2015-08-18 12:04:34 -0400

    Adding Promises API support

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var getParamNames = require('get-parameter-names');
+var Promise = require('any-promise');
 
 module.exports = Canister;
 
 function Canister(resolvers, context) {
-  if (!context) {
-    context = {};
-  }
+  this.context = context || {};
+  this.resolvers = [].concat(resolvers);
+}
 
-  if (!Array.isArray(resolvers)) {
-    resolvers = [resolvers];
-  }
+Canister.prototype.run = function (fn, cb) {
+  var resolvers = this.resolvers;
+  var context = this.context;
 
-  this.run = function(fn, cb) {
-    cb = cb || function() {};
-
+  var result = new Promise(function(resolve, reject) {
     var dependencies = [];
     var unmetDependencies = [];
     var synchronous = true;
 
     var paramNames = getParamNames(fn);
 
-    paramNames.forEach(function(paramName, index) {
+    paramNames.forEach(function (paramName, index) {
       if (paramName === 'cb') {
         synchronous = false;
-        dependencies.push(cb);
+        dependencies.push(function (err, value) {
+          if (err) return reject(err);
+
+          resolve(value);
+        });
       } else if (paramName) {
-        var depValue = resolve(resolvers, paramName, index);
+        var depValue = resolveDependency(resolvers, paramName, index);
         if (depValue !== null && depValue !== undefined) {
           dependencies.push(depValue);
         } else {
@@ -35,34 +38,40 @@ function Canister(resolvers, context) {
     });
 
     if (unmetDependencies.length > 0)
-      return cb(new Error('unmet dependencies: ' + unmetDependencies.join(',')));
+      return reject(new Error('unmet dependencies: ' + unmetDependencies.join(',')));
 
     var result = fn.apply(context, dependencies);
 
     // If result is a promise, then resolve it and call callback
     if (result && result.then) {
-      result.then(function(value) {
-        return cb(null, value);
-      }, cb);
+      result.then(resolve, reject);
     } else if (synchronous) {
-      return cb(null, result);
+      return resolve(result);
     }
-  };
+  });
 
-  function resolve(resolvers, name, index) {
-    for (var i = 0; i < resolvers.length; i++) {
-      var resolver = resolvers[i];
+  if (cb) {
+    result.then(function (value) {
+      cb(null, value);
+    }, cb);
+  } else {
+    return result;
+  }
+};
 
-      var resolution;
-      if (typeof resolver === 'function') {
-        resolution = resolver(name, index);
-      } else if (typeof resolver === 'object') {
-        resolution = resolver[name];
-      }
+function resolveDependency(resolvers, name, index) {
+  for (var i = 0; i < resolvers.length; i++) {
+    var resolver = resolvers[i];
 
-      if (resolution !== null && resolution !== undefined) {
-        return resolution;
-      }
+    var resolution;
+    if (typeof resolver === 'function') {
+      resolution = resolver(name, index);
+    } else if (typeof resolver === 'object') {
+      resolution = resolver[name];
+    }
+
+    if (resolution !== null && resolution !== undefined) {
+      return resolution;
     }
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,131 +1,139 @@
-var assert = require('chai').assert;
-var Promise = require('bluebird');
+var Promise = require('any-promise');
 var Canister = require('../index.js');
 
-describe('canister', function() {
-  it('callsback with error if unmet dependencies', function(done) {
-    new Canister(function(name) {
-      // nothing ever resolves
-    }).run(function(a) {
-      assert.fail('should never be called since a is unmet dependency');
-    }, function(err) {
+var test = require('tape');
+
+test('callsback with error if unmet dependencies', function (t) {
+  new Canister(function (name) {
+    // nothing ever resolves
+  }).run(function (a) {
+      t.fail('should never be called since a is unmet dependency');
+    }, function (err) {
       //assert.equal(typeof err, 'Error');
-      assert.equal(err.message, 'unmet dependencies: a');
-      done();
+      t.equal(err.message, 'unmet dependencies: a');
+      t.end();
     });
-  });
-
-  it('handles empty injection', function(done) {
-    new Canister(function(name) {
-      assert.fail('should not be called');
-    }).run(function(/* NO ARGS HERE*/) {
-      assert.equal(arguments.length, 0);
-      return 10;
-    }, function(err, result) {
-      assert(!err);
-      assert.equal(result, 10);
-      done();
-    });
-  });
-
-	it('passes index to resolver', function(done) {
-    new Canister(function(name, index) {
-			assert(typeof index === 'number');
-      if (name === 'a') return index;
-      if (name === 'b') return index;
-    }).run(function(a, b) {
-			assert.equal(a, 0);
-			assert.equal(b, 1);
-		}, function(err, result) {
-			assert(!err, err);
-			done();
-		});
-	});
-
-  it('handles simple injection', function(done) {
-    new Canister(function(name) {
-      if (name === 'a') return 'a';
-      if (name === 'b') return 'b';
-    }).run(function(a, b) {
-      assert.equal(arguments.length, 2);
-      return a + b;
-    }, function(err, result) {
-      assert.equal(result, 'ab');
-      done();
-    });
-  });
-
-  it('handles simple injection with hash', function(done) {
-    new Canister({a: 'a', b: 'b'}).run(function(a, b) {
-      assert.equal(arguments.length, 2);
-      return a + b;
-    }, function(err, result) {
-      assert.equal(result, 'ab');
-      done();
-    });
-  });
-
-  it('support not providing a callback to run', function(done) {
-    new Canister(function() {
-      assert.fail('should not be run');
-    }).run(function() {
-      done();
-    });
-  });
-
-  it('supports binding contexts', function(done) {
-    new Canister(function() {
-      assert.fail('should not be run');
-    }, {a: 10}).run(function() {
-      assert.equal(this.a, 10);
-      done();
-    });
-  });
-
-  it('runs functions with callbacks properly', function(done) {
-    new Canister(function(name) {
-      if (name === 'a') return 'a';
-      if (name === 'b') return 'b';
-      if (name === 'cb') return function(err, val) {
-        assert(val === 'c');
-        done(err);
-      };
-    }).run(function(a, b, cb) {
-      assert.equal(arguments.length, 3);
-      cb(null, 'c');
-    }, function(err, result) {
-      assert(!err);
-      assert.equal(result, 'c');
-      done();
-    });
-  });
-
-  it('supports returning a Promise from function', function(done) {
-    new Canister(function(name) {
-      assert.fail('should not be run');
-    }).run(function() {
-      return Promise.resolve('test');
-    }, function(err, result) {
-      assert(!err);
-      assert.equal(result, 'test');
-      done();
-    });
-  });
-
-  it('supports an array of resolvers', function(done) {
-    new Canister([
-      function(name) {
-        if (name === 'a') return 'a';
-      },
-      function(name) {
-        if (name === 'b') return 'b';
-      }
-    ]).run(function(a, b) {
-      return a+b;
-    }, function(err, result) {
-      assert(!err, err);
-      assert.equal(result, 'ab');
-      done();
-    })
-  })
 });
+
+test('handles empty injection', function (t) {
+  new Canister(function (name) {
+    t.fail('should not be called');
+  }).run(function (/* NO ARGS HERE*/) {
+      t.equal(arguments.length, 0);
+      return 10;
+    }, function (err, result) {
+      t.error(err);
+      t.equal(result, 10);
+      t.end();
+    });
+});
+
+test('passes index to resolver', function (t) {
+  new Canister(function (name, index) {
+    t.equal(typeof index, 'number');
+    if (name === 'a') return index;
+    if (name === 'b') return index;
+  }).run(function (a, b) {
+      t.equal(a, 0);
+      t.equal(b, 1);
+    }, function (err, result) {
+      t.error(err);
+      t.end();
+    });
+});
+
+test('handles simple injection', function (t) {
+  new Canister(function (name) {
+    if (name === 'a') return 'a';
+    if (name === 'b') return 'b';
+  }).run(function (a, b) {
+      t.equal(arguments.length, 2);
+      return a + b;
+    }, function (err, result) {
+      t.equal(result, 'ab');
+      t.end();
+    });
+});
+
+test('handles simple injection with hash', function (t) {
+  new Canister({a: 'a', b: 'b'}).run(function (a, b) {
+    t.equal(arguments.length, 2);
+    return a + b;
+  }, function (err, result) {
+    t.equal(result, 'ab');
+    t.end();
+  });
+});
+
+test('support not providing a callback to run', function (t) {
+  new Canister(function () {
+    t.fail('should not be run');
+  }).run(function () {
+      t.end();
+    });
+});
+
+test('supports binding contexts', function (t) {
+  new Canister(function () {
+    t.fail('should not be run');
+  }, {a: 10}).run(function () {
+      t.equal(this.a, 10);
+      t.end();
+    });
+});
+
+test('runs functions with callbacks properly', function (t) {
+  new Canister(function (name) {
+    if (name === 'a') return 'a';
+    if (name === 'b') return 'b';
+    if (name === 'cb') return function (err, val) {
+      return val;
+    };
+  }).run(function (a, b, cb) {
+      t.equal(arguments.length, 3);
+      cb(null, 'c');
+    }, function (err, result) {
+      t.error(err);
+      t.equal(result, 'c');
+      t.end();
+    });
+});
+
+test('supports returning a Promise from function', function (t) {
+  new Canister(function (name) {
+    t.fail('should not be run');
+  }).run(function () {
+      return Promise.resolve('test');
+    }, function (err, result) {
+      t.error(err);
+      t.equal(result, 'test');
+      t.end();
+    });
+});
+
+test('supports an array of resolvers', function (t) {
+  new Canister([
+    function (name) {
+      if (name === 'a') return 'a';
+    },
+    function (name) {
+      if (name === 'b') return 'b';
+    }
+  ]).run(function (a, b) {
+      return a + b;
+    }, function (err, result) {
+      t.error(err, err);
+      t.equal(result, 'ab');
+      t.end();;
+    })
+});
+
+test('supports promise API', function(t) {
+  new Canister({a: 10}).run(function(a) {
+    return a;
+  }).then(function(result) {
+    t.equal(result, 10);
+    t.end();
+  });
+});
+
